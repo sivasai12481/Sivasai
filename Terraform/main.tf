d225e06c2dfbe28bdcf32626c7b17926efe5a0d7
Siva Sai <siva.rayavaram@nagra.com>
2025-06-11 11:48:40 +0530

    updating terraform code

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,4 +11,20 @@ resource "aws_instance" "example" {
 
 resource "aws_api_gateway_api_key" "example" {
   name = "example"
+<<<<<<< Updated upstream
+=======
+  tags = {
+    name: "Instance"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+>>>>>>> Stashed changes
 }
